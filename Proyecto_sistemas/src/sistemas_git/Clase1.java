b7ca1483c0329848447b856d1376d38e4274e77b
urtzi <urtzi@Talos>
2017-04-30 17:14:10 +0200

    deberia fallar

--- Proyecto_sistemas/src/sistemas_git/Clase1.java
+++ Proyecto_sistemas/src/sistemas_git/Clase1.java
@@ -5,7 +5,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
 		
-		System.out.println("Adios, world");
+		System.out.println("Adios world");
 		
 	}
 
